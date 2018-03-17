cea424a1795addf777fa77fbfe66bba2dc7005a8
Srini <srini@coursera-something.com>
2018-03-17 13:49:21 +0530

    modified files

--- assignment1-solution/app.js
+++ assignment1-solution/app.js
@@ -6,9 +6,23 @@
 LunchCheckController.$inject = ['$scope'];
 
 function LunchCheckController($scope) {
-$scope.list="";
-$scope.message="test";
-}
+  $scope.list="";
+  $scope.message="";
+  $scope.CheckList = function (list) {
+    var spaceComma = /\s*,\s*/; // to seperate comma along with immediate before and after optional spaces
+    var listArray = list.split(spaceComma);
+    listArray = listArray.filter(Boolean); // to remove empty string
+    var count = listArray.length;
+    $scope.test = listArray;
+    if(count>3)
+    {
+      $scope.message="Too much!";
+    }
+    else {
+      $scope.message="Enjoy!";
+    }
+  }
 
+}
 
 })();
