492db9456251281dd00f6b52f16642e73f108a56
Srini <srini@coursera-something.com>
2018-03-17 15:32:49 +0530

    Final code

--- assignment1-solution/app.js
+++ assignment1-solution/app.js
@@ -8,18 +8,24 @@ LunchCheckController.$inject = ['$scope'];
 function LunchCheckController($scope) {
   $scope.list="";
   $scope.message="";
+  $scope.msgColor="";
   $scope.CheckList = function (list) {
     var spaceComma = /\s*,\s*/; // to seperate comma along with immediate before and after optional spaces
     var listArray = list.split(spaceComma);
     listArray = listArray.filter(Boolean); // to remove empty string
     var count = listArray.length;
-    $scope.test = listArray;
-    if(count>3)
+    if(count==0)
     {
-      $scope.message="Too much!";
+      $scope.message="Please enter data first";
+      $scope.msgColor="red";
     }
-    else {
+    else if(count<=3) {
       $scope.message="Enjoy!";
+      $scope.msgColor="green";
+    }
+    else{
+      $scope.message="Too much!";
+      $scope.msgColor="green";
     }
   }
 
